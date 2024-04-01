75a725c6c42a1c96ac8ec9dd3f72e173af0b947a
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2024-04-01 20:02:34 +0530

    project uploaded

---
+++ Jenkins_Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jenkins_Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return Ok(result);
+        }
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return Ok(result);
+        }
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return Ok(result);
+        }
+
+        [HttpGet("divide")]
+        public IActionResult Divide(double num1, double num2)
+        {
+            if(num2 == 0)
+            {
+                return BadRequest("Cannot divide by Zero");
+            }
+
+            double result = num1 / num2;
+            return Ok(result);
+        }
+    }
+}
---
+++ Jenkins_Test/JenkinsCalucaltorTest.cs
@@ -0,0 +1,61 @@
+﻿using Jenkins_Calculator.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_Test
+{
+    [TestFixture]
+    public class JenkinsCalucaltorTest
+    {
+        [Test]
+        public void TestAdd()
+        {
+            var controller = new CalculatorController();
+
+            var result = controller.Add(3, 4) as OkObjectResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(7, result.Value);
+        }
+
+        [Test]
+        public void TestSubtract()
+        {
+            var controller = new CalculatorController();
+            var result = controller.Subtract(7, 4) as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Value);
+        }
+
+        [Test]
+        public void TestMultiply()
+        {
+            var controller = new CalculatorController();
+            var result = controller.Multiply(7, 4) as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(28, result.Value);
+        }
+
+        [Test]
+        public void TestDivide()
+        {
+            var controller = new CalculatorController();
+            var result = controller.Divide(8, 4) as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Value);
+        }
+
+        [Test]
+        public void TestDivideByZero()
+        {
+            var controller = new CalculatorController();
+            var result = controller.Divide(7, 0) as OkObjectResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Cannot divide by zero", result.Value);
+        }
+    }
+}
